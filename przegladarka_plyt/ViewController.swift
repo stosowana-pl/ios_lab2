3778ce91db4395bf40bbcd839802fc07b607840d
Użytkownik Gość <Guest@imac-01.local>
2017-10-10 08:39:43 +0200

    initial commit

---
+++ przegladarka_plyt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  przegladarka_plyt
+//
+//  Created by Użytkownik Gość on 10.10.2017.
+//  Copyright © 2017 Użytkownik Gość. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
