3a7eb5f4bb25f1ebf596289033fe2904467c6c83
Użytkownik Gość <Guest@imac-18.local>
2017-10-17 12:21:07 +0200

    da

---
+++ przegladarka_plyt/MusicRecord.swift
@@ -0,0 +1,9 @@
+//
+//  MusicRecord.swift
+//  przegladarka_plyt
+//
+//  Created by Użytkownik Gość on 17.10.2017.
+//  Copyright © 2017 Użytkownik Gość. All rights reserved.
+//
+
+import Foundation
