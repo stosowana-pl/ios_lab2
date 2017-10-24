54664bbc48a723f5983d4af770d18d2b08849e69
Avasil <avasil@Avasils-Mac.local>
2017-10-24 11:03:45 +0200

    dada

--- przegladarka_plyt/MusicRecord.swift
+++ przegladarka_plyt/MusicRecord.swift
@@ -2,8 +2,25 @@
 //  MusicRecord.swift
 //  przegladarka_plyt
 //
-//  Created by Użytkownik Gość on 17.10.2017.
+//  Created by Avasil on 24/10/2017.
 //  Copyright © 2017 Użytkownik Gość. All rights reserved.
 //
 
 import Foundation
+
+class MusicRecord {
+    let artist: String
+    let album: String
+    let genre: String
+    let year: Int
+    let tracks: Int
+    
+    init(_ artist: String, _ album: String, _ genre: String, _ year: Int, _ tracks: Int) {
+        
+        self.artist = artist
+        self.album = album
+        self.genre = genre
+        self.year = year
+        self.tracks = tracks
+    }
+}
--- przegladarka_plyt/ViewController.swift
+++ przegladarka_plyt/ViewController.swift
@@ -9,10 +9,122 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var musicRecords = [MusicRecord]()
+    var currentRecord: Int = 0
+    
+    func getTotalNumber() -> Int {
+        return musicRecords.count
+    }
+    
+    @IBOutlet weak var SaveButton: UIButton!
+    
+    @IBOutlet weak var NewRecord: UIButton!
+    
+    @IBOutlet weak var RemoveRecord: UIButton!
+    
+    @IBOutlet weak var PreviousRecord: UIButton!
+    
+    @IBOutlet weak var NextRecord: UIButton!
+    
+    @IBOutlet weak var ArtistField: UITextField!
+    
+    @IBOutlet weak var TitleField: UITextField!
+    
+    @IBOutlet weak var GenreField: UITextField!
+    
+    @IBOutlet weak var YearField: UITextField!
+    
+    @IBOutlet weak var TracksField: UITextField!
+    
+    @IBOutlet weak var RecordXY: UILabel!
+    
+    @IBAction func SaveClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func NewRecordClicked(_ sender: UIButton) {
+        let artist = ArtistField.text ?? ""
+        let title = TitleField.text ?? ""
+        let genre = GenreField.text ?? ""
+        let year = Int(YearField.text ?? "") ?? 0
+        let tracks = Int(TracksField.text ?? "") ?? 0
+        
+        let newRecord = MusicRecord(artist, title, genre, year, tracks)
+        
+        self.musicRecords.append(newRecord)
+        
+        self.updateView()
+    }
+    
+    @IBAction func RemoveRecordClicked(_ sender: UIButton) {
+        self.musicRecords.remove(at: self.currentRecord)
+        self.updateView()
+    }
+    
+    @IBAction func PreviousRecordClicked(_ sender: UIButton) {
+        self.currentRecord -= 1
+        self.updateView()
+    }
+    
+    @IBAction func NextRecordClicked(_ sender: UIButton) {
+        self.currentRecord += 1
+        self.updateView()
+    }
+    
+    
+    func parseJson(_ json: [String: Any]) -> MusicRecord? {
+        guard let artist = json["artist"] as? String,
+        let album = json["album"] as? String,
+        let genre = json["genre"] as? String,
+        let year = json["year"] as? Int,
+        let tracks = json["tracks"] as? Int
+            else {
+                return nil
+        }
+        return MusicRecord(artist, album, genre, year, tracks)
+    }
+    
+    func requestData() {
+        let session = URLSession.shared
+        let url = URL.init(string: "https://isebi.net/albums.php")
+        
+        session.dataTask(with: url!, completionHandler: { (maybeData: Data?, _, _) in
+            if let data = maybeData,
+            let maybeJson = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]],
+                let json = maybeJson {
+                for singleRecord in json {
+                    if let newRecord = self.parseJson(singleRecord){
+                        self.musicRecords.append(newRecord)
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                self.updateView()
+            }
+    }).resume()
+    }
+    
+    func updateView(){
+        // turned off when we are at first record
+        self.PreviousRecord.isEnabled = self.currentRecord > 0
+        // turned off when we are at last record
+        self.NextRecord.isEnabled = self.currentRecord < self.getTotalNumber()
+        
+        if(self.currentRecord != self.getTotalNumber()){
+        let currentRecord = self.musicRecords[self.currentRecord]
+        self.ArtistField.text = currentRecord.artist
+        self.TitleField.text = currentRecord.album
+        self.GenreField.text = currentRecord.genre
+        self.YearField.text = String(currentRecord.year)
+        self.TracksField.text = String(currentRecord.tracks)
+        
+        self.RecordXY.text = "Rekord \(self.currentRecord + 1) z \(self.getTotalNumber())"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.requestData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +132,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
